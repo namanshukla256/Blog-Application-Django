2aa11bceccca97230104954f19fdacd594bd1bf5
Naman Shukla <namanshukla256@gmail.com>
2025-05-07 15:33:17 +0530

    initial setup done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,36 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # Our Custom Manager
 
     class Meta:
         ordering = ['-publish']
