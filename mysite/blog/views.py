282ece94ac2500cb14634f32edcfddfcf1b4a627
Naman Shukla <namanshukla256@gmail.com>
2025-05-09 21:00:17 +0530

    Mission2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,20 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
-
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagnation with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(
         request,
         'blog/post/list.html',
@@ -12,12 +22,14 @@ def post_list(request):
     )
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
-    )
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
